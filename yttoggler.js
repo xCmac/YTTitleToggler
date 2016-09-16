59b2d20a54931793df5665a50b9b88e7aa2eae69
xCmac <dsemac@gmail.com>
2016-09-16 10:47:22 -0500

    fixed toggling bug

--- yttoggler.js
+++ yttoggler.js
@@ -1,6 +1,7 @@
-var $toggler = $("<input id=\"cb3\" type=\"checkbox\" class=\"tgl tgl-skewed\"/><label data-tg-off=\"OFF\" data-tg-on=\"ON\" for=\"cb3\" class=\"tgl-btn\"></label>", {id: "yttoggler"});
-$toggler.click(function(){
-	$("#watch-headline-title").toggle("slow");
+var $toggler = $("<input id=\"cb3\" type=\"checkbox\" class=\"tgl tgl-skewed\"/><label data-tg-off=\"HIDE\" data-tg-on=\"SHOW\" for=\"cb3\" class=\"tgl-btn\"></label>", {id: "yttoggler"});
+
+$toggler.change(function() {
+	$("#watch-headline-title").toggle();        
 });
 
 $("#watch-header").prepend($toggler);
