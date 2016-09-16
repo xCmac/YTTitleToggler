65ab59f71660b7340a7b9cc175cbbcaa1ec9b30c
xCmac <dsemac@gmail.com>
2016-09-16 01:48:27 -0500

    Added an intuitive button to toggle the title, still need to fix a bug that keeps it showing

--- yttoggler.js
+++ yttoggler.js
@@ -1,6 +1,6 @@
-var $div = $("<div><p>YTToggler</p></div>", {id: "yttoggler"});
-$div.click(function(){
+var $toggler = $("<input id=\"cb3\" type=\"checkbox\" class=\"tgl tgl-skewed\"/><label data-tg-off=\"OFF\" data-tg-on=\"ON\" for=\"cb3\" class=\"tgl-btn\"></label>", {id: "yttoggler"});
+$toggler.click(function(){
 	$("#watch-headline-title").toggle("slow");
 });
 
-$("#watch-header").prepend($div);
+$("#watch-header").prepend($toggler);
